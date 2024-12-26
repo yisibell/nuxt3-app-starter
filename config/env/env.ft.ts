41f54a4303e13c2e5e0d2f0e7b010e924bbbbc16
wenqing <wenqing@kerrylan.com>
2024-12-26 09:29:46 +0800

    chore: update config

--- config/env/env.ft.ts
+++ config/env/env.ft.ts
@@ -1,12 +1,12 @@
 export default {
   NUXT_APP_ENV: 'ft',
-  // NUXT_APP_BASE_API: 'http://16.163.143.182',
+  // NUXT_APP_BASE_API: '',
 
   NUXT_APP_BASE_API: '/api2',
   proxy: {
     options: [
       {
-        target: 'http://16.163.143.182',
+        target: '',
         pathRewrite: { '/api2': '' },
         pathFilter: ['/api2/**'],
       },
