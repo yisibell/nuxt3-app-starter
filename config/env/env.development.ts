3691e4ff58fd2d720ba12b9154c800da6891deef
wenqing <wenqing@kerrylan.com>
2025-02-07 17:01:53 +0800

    fix: proxy demo

--- config/env/env.development.ts
+++ config/env/env.development.ts
@@ -1,15 +1,15 @@
 export default {
   NUXT_APP_ENV: 'development',
-  // NUXT_APP_BASE_API: '',
+  NUXT_APP_BASE_API: '',
 
-  NUXT_APP_BASE_API: '/api2',
-  proxy: {
-    options: [
-      {
-        target: '',
-        pathRewrite: { '/api2': '' },
-        pathFilter: ['/api2/**'],
-      },
-    ],
-  },
+  // NUXT_APP_BASE_API: '/api2',
+  // proxy: {
+  //   options: [
+  //     {
+  //       target: '',
+  //       pathRewrite: { '/api2': '' },
+  //       pathFilter: ['/api2/**'],
+  //     },
+  //   ],
+  // },
 }
